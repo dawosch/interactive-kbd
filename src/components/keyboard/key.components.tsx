86f46871d5b2bc2da87ff831609d388e15232b21
Dawosch <57100280+dawosch@users.noreply.github.com>
2024-11-28 20:32:03 +0100

    Improvements

--- src/components/keyboard/key.components.tsx
+++ src/components/keyboard/key.components.tsx
@@ -3,7 +3,7 @@ import React, { useEffect, useRef } from 'react';
 import './styles.css';
 import { KEYCODE_ICONS, KEYCODE_LEGENDS } from '../../keycodes';
 
-function getLegendFromKeycode(keycode?: string) {
+function getLegendFromKeycode(keycode?: string, shift?: boolean) {
   if (!keycode) return;
 
   // Check if keycode is QMK function
@@ -17,8 +17,8 @@ function getLegendFromKeycode(keycode?: string) {
 
   // Check if keycode is mapped to legend
   const prefix = _keycode.substring(0, _keycode.indexOf('_'));
-  const legend = KEYCODE_LEGENDS[prefix as keyof typeof KEYCODE_LEGENDS]?.find((map) => map.key === _keycode || map.aliases?.includes(_keycode))?.legend;
-  if (legend !== undefined) return legend; // Empty string should be printed
+  const key = KEYCODE_LEGENDS[prefix as keyof typeof KEYCODE_LEGENDS]?.find((map) => map.key === _keycode || map.aliases?.includes(_keycode));
+  if (key !== undefined) return shift && key.shift ? key.shift : key.legend; // Empty string should be printed
 
   return _keycode;
 }
@@ -33,31 +33,53 @@ function extractLayer(keycode: string) {
   return parseInt(match[0], 10);
 }
 
+function changeBaseLayer(keycode: string) {
+  return keycode.indexOf('DF') !== -1; // TODO: Make identification more generic
+}
+
 type KeyProps = {
   keycode?: string;
+  shift?: boolean;
   positionX: number;
   positionY: number;
   width: number;
   height: number;
   pressed?: boolean;
-  onPressed: (layer: number, changeBaseLayer: boolean) => void;
+  onPressed: (layer?: number, advaced?: { changeBaseLayer?: boolean; shift?: boolean }) => void;
   // onReleased: () => void;
 };
 
-export function Key({ keycode, positionX, positionY, width, height, pressed, onPressed }: KeyProps) {
+export function Key({ keycode, shift, positionX, positionY, width, height, pressed, onPressed }: KeyProps) {
   const wasPressedBefore = useRef<boolean | undefined>(pressed);
   const baseKeycode = useRef<string | undefined>(keycode);
 
   // TODO: All keys will be rerendered on layer change, so this method doesn't work
   useEffect(() => {
-    if (pressed && !wasPressedBefore.current && keycode && keycodeContainsLayer(keycode)) {
-      baseKeycode.current = keycode;
-      onPressed(extractLayer(keycode), false); // TODO: "false" should be genericextractLayer(keycode)
+    switch (true) {
+      case pressed && !wasPressedBefore.current && keycode && keycodeContainsLayer(keycode): // Layer key pressed
+        baseKeycode.current = keycode;
+        onPressed(extractLayer(keycode), { changeBaseLayer: changeBaseLayer(keycode) }); // TODO: "false" should be genericextractLayer(keycode)
+        break;
+      case !pressed && wasPressedBefore.current && baseKeycode.current && keycodeContainsLayer(baseKeycode.current): // Layer key released
+        baseKeycode.current = keycode;
+        onPressed(); // TODO: When base layer will be changed we don't have to overwrite it with 0
+        break;
+      case pressed && keycode === 'KC_LSFT':
+        onPressed(0, { shift: true });
+        break;
+      case !pressed && keycode === 'KC_LSFT':
+        onPressed(0, { shift: false });
+        break;
     }
-    if (!pressed && wasPressedBefore.current && baseKeycode.current && keycodeContainsLayer(baseKeycode.current)) {
-      baseKeycode.current = keycode;
-      onPressed(0, false);
-    }
+
+    // if (pressed && !wasPressedBefore.current && keycode && keycodeContainsLayer(keycode)) {
+    //   baseKeycode.current = keycode;
+    //   onPressed(extractLayer(keycode), false); // TODO: "false" should be genericextractLayer(keycode)
+    // }
+    // if (!pressed && wasPressedBefore.current && baseKeycode.current && keycodeContainsLayer(baseKeycode.current)) {
+    //   baseKeycode.current = keycode;
+    //   onPressed(0, false);
+    // }
 
     wasPressedBefore.current = pressed ?? false;
   }, [keycode, pressed, onPressed]);
@@ -72,7 +94,7 @@ export function Key({ keycode, positionX, positionY, width, height, pressed, onP
         height,
       }}
     >
-      <span className="legend">{getLegendFromKeycode(keycode)}</span>
+      <span className="legend">{getLegendFromKeycode(keycode, shift)}</span>
     </div>
   );
 }
--- src/components/keyboard/keyboard.component.tsx
+++ src/components/keyboard/keyboard.component.tsx
@@ -7,9 +7,18 @@ import { calculateParentSize, matrixToId } from './keyboard.utils';
 type KeyboardProps = { keys: QmkKey[]; keymap?: QmkKeymap; keyWidth: number; keyHeight: number; space: number };
 
 export function Keyboard({ keys, keymap, keyWidth, keyHeight, space }: KeyboardProps) {
-  const baseLayer = 0; // TODO: The "onPressed" function should return a layer AND if it's changing "baseLayer"
+  let baseLayer = 0; // TODO: The "onPressed" function should return a layer AND if it's changing "baseLayer"
   const [layer, setLayer] = useState<number>(baseLayer);
   const [width, height] = useMemo(() => calculateParentSize(keys ?? [], keyWidth, keyHeight, space), [keys, keyHeight, keyWidth, space]);
+  const [shift, setShift] = useState<boolean>();
+
+  const onKeyboardKeyPressed = (layer?: number, advanced?: { changeBaseLayer?: boolean; shift?: boolean }) => {
+    // TODO: BaseLayer not working
+    console.log('layer', layer);
+    layer ? setLayer(layer) : setLayer(baseLayer);
+    if (advanced?.changeBaseLayer) baseLayer = layer!;
+    if (advanced?.shift !== undefined) setShift(shift);
+  };
 
   return (
     <div className="keyboard" style={{ width, height }}>
@@ -17,14 +26,13 @@ export function Keyboard({ keys, keymap, keyWidth, keyHeight, space }: KeyboardP
         <Key
           key={matrixToId(key.matrix)}
           keycode={keymap?.[layer][i]}
-          // legend={getLegend(layer, key.matrix, keymap)}
-          // icon={getIcon(layer, key.matrix, keymap)}
+          shift={shift}
           positionX={keyWidth * key.x + space * Math.trunc(key.x)}
           positionY={keyHeight * key.y + space * Math.trunc(key.y)}
           width={keyWidth * (key.w ?? 1)}
           height={keyHeight * (key.h ?? 1)}
           pressed={key.pressed}
-          onPressed={setLayer}
+          onPressed={onKeyboardKeyPressed}
           // onReleased={() => console.log('Released', key.keycode)}
         />
       ))}
